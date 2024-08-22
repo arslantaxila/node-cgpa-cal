3fcc5f77e56e241768a1ef8f3a47b999136d8f02
Hassan <hasantahir214@gmail.com>
2024-08-22 17:23:54 +0500

    added database file

---
+++ cgpa_cal.sql
@@ -0,0 +1,266 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cgpa_cal
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `semester_id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `code` varchar(45) NOT NULL,
+  `credit_hour` float NOT NULL DEFAULT '0',
+  `marks` float NOT NULL,
+  `gpa` float NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `created_by` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_semester_id_idx` (`semester_id`),
+  CONSTRAINT `fk_semester_id` FOREIGN KEY (`semester_id`) REFERENCES `semester` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grade`
+--
+
+DROP TABLE IF EXISTS `grade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grade` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `letter` varchar(2) NOT NULL,
+  `point` float NOT NULL,
+  `min` int NOT NULL,
+  `max` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grade`
+--
+
+LOCK TABLES `grade` WRITE;
+/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
+INSERT INTO `grade` VALUES (1,'A',4,85,100),(2,'A-',3.7,80,84),(3,'B+',3.3,75,79),(4,'B',3,70,74),(5,'B-',2.7,65,69),(6,'C+',2.3,60,64),(7,'C',2,55,59),(8,'C-',1.7,50,54),(9,'D+',1.3,45,49),(10,'D',1,40,44),(11,'F',0,0,39);
+/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `right`
+--
+
+DROP TABLE IF EXISTS `right`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `right` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `display_name` varchar(45) DEFAULT NULL,
+  `sub_heading` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `right`
+--
+
+LOCK TABLES `right` WRITE;
+/*!40000 ALTER TABLE `right` DISABLE KEYS */;
+INSERT INTO `right` VALUES (1,'transcript','Transcript Request',0),(2,'gpa-calculation','GPA Calculation',0),(3,'user','User',0);
+/*!40000 ALTER TABLE `right` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (2,'Admin'),(3,'Student'),(1,'Superadmin');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_right_permission`
+--
+
+DROP TABLE IF EXISTS `role_right_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_right_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `right_id` int NOT NULL,
+  `read` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `create` enum('yes','no') NOT NULL DEFAULT 'no',
+  `update` enum('yes','no') NOT NULL DEFAULT 'no',
+  `delete` enum('yes','no') NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_role_right` (`role_id`,`right_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_right_permission`
+--
+
+LOCK TABLES `role_right_permission` WRITE;
+/*!40000 ALTER TABLE `role_right_permission` DISABLE KEYS */;
+INSERT INTO `role_right_permission` VALUES (1,1,1,'yes','yes','yes','yes'),(2,1,2,'yes','yes','yes','yes'),(3,1,3,'yes','yes','yes','yes'),(4,2,1,'yes','no','yes','yes'),(5,3,1,'yes','yes','no','no'),(6,3,2,'yes','yes','yes','yes');
+/*!40000 ALTER TABLE `role_right_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `semester`
+--
+
+DROP TABLE IF EXISTS `semester`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `semester` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `gpa` float NOT NULL,
+  `user_id` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `created_by` int NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq` (`name`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `semester`
+--
+
+LOCK TABLES `semester` WRITE;
+/*!40000 ALTER TABLE `semester` DISABLE KEYS */;
+/*!40000 ALTER TABLE `semester` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transcript`
+--
+
+DROP TABLE IF EXISTS `transcript`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transcript` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `type` enum('Interim Transcript','Final Transcript') NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cnic` varchar(13) NOT NULL,
+  `mobile` varchar(11) NOT NULL,
+  `status` enum('Pending','Underprocess','Ready to Collect','Complete') NOT NULL DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL,
+  `created_by` int NOT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transcript`
+--
+
+LOCK TABLES `transcript` WRITE;
+/*!40000 ALTER TABLE `transcript` DISABLE KEYS */;
+INSERT INTO `transcript` VALUES (3,3,'Interim Transcript','hassantahir143@gmail.com','3131314415151','03414198498','Underprocess','2024-08-17 13:48:30',3,'2024-08-17 13:49:47',2),(4,18,'Final Transcript','hassan@lmkt.com','9489247927498','03829478297','Complete','2024-08-22 06:23:19',18,'2024-08-22 06:24:35',2);
+/*!40000 ALTER TABLE `transcript` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `enrollment` varchar(50) DEFAULT NULL,
+  `cnic` varchar(13) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `enrollment_UNIQUE` (`enrollment`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Super Admin','hasantahir214@gmail.com','1',NULL,'$2a$10$JNz5Y8KvijwNI0eKIhYg5.W47zBp2Zs39zSL1zM07cidBMyEeC24u',1,'2024-06-15 21:09:22',1,'2024-07-07 11:21:35',11),(2,'Admin','admin@gpa.com','2',NULL,'$2a$10$BoYFFZ5ZUv/eY5u3rmUfi.JxABT4dp4n9SFo8.jHXv0pXj2.NqQO2',2,'2024-08-15 11:17:02',1,'2024-08-17 13:45:37',1),(3,'Student','student@gpa.com','3',NULL,'$2a$10$mqIrGHqZlxrCmz8GTroWtO/dG.hps93wCkGVNC90ehoa02UW6Jvhu',3,'2024-08-17 13:46:01',1,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-22 17:22:53
